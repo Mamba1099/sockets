13025c04842f751ae4eb6f2b8978a05c6845ce1c
Mamba1099 <mambakaranja240@gmail.com>
2023-07-07 16:43:58 +0300

    learning python threading module

--- Threading/mult_threading.py
+++ Threading/mult_threading.py
@@ -1,21 +1,21 @@
 import time
 import threading
 
-def calc_square(numbers):
+def calc_square_area(numbers):
     print('calculate the square of numbers')
     for n in numbers:
         time.sleep(0.2)
         print('square:', n*n)
-
+        
 def calc_cube(numbers):
     print('calculate the cube of numbers')
     for n in numbers:
         time.sleep(0.2)
-        print('cube:', n*n*n)
+        print('cube:', n*n*n) 
 
 arr = [2, 4, 5, 8, 9]
 t = time.time()
-t1 = threading.Thread(target = calc_square, args =(arr,))
+t1 = threading.Thread(target = calc_square_area, args =(arr,))
 t2 = threading.Thread(target = calc_cube, args =(arr,))
 
 t1.start()
